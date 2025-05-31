df1266b1e5add8bb9c327850b5b8283c6245acbf
Shradinx <luphcore@gmail.com>
2025-05-30 23:20:41 -0400

    overhaul turtle code

--- src/turtle/websocket.lua
+++ src/turtle/websocket.lua
@@ -1,158 +1,241 @@
+action_tbl = {
+    ["inspect"] = turtle.inspect,
+    ["inspect_up"] = turtle.inspectUp,
+    ["inspect_down"] = turtle.inspectDown,
+    ["dig"] = { turtle.inspect, turtle.dig },
+    ["dig_up"] = { turtle.inspectUp, turtle.digUp },
+    ["dig_down"] = { turtle.inspectDown, turtle.digDown },
+    ["forward"] = turtle.forward,
+    ["back"] = turtle.back,
+    ["up"] = turtle.up,
+    ["down"] = turtle.down,
+    ["turn_left"] = turtle.turnLeft,
+    ["turn_right"] = turtle.turnRight,
+    ["place"] = turtle.place,
+    ["place_up"] = turtle.placeUp,
+    ["place_down"] = turtle.placeDown
+}
+
+local function concatMessage(text, msg)
+    if msg == nil then
+        return text
+    end
+    return text .. "|" .. msg
+end
+
+local function inspectAction(ws, direction)
+    local func = action_tbl[direction]
+    local exists, data = func()
+    local msg = direction .. "|" .. textutils.serialiseJSON(data)
+    ws.send(msg)
+end
+
+local function inspectForward(ws)
+    inspectAction(ws, "inspect")
+end
+
+local function inspectDown(ws)
+    inspectAction(ws, "inspect_down")
+end
+
+local function inspectUp(ws)
+    inspectAction(ws, "inspect_up")
+end
+
+local function turnAction(ws, direction)
+    local func = action_tbl[direction]
+    local turn, msg = func()
+    local text = direction .. "|" .. tostring(turn)
+    ws.send(concatMessage(text, msg))
+end
+
+local function turnLeftAction(ws)
+    turnAction(ws, "turn_left")
+end
+
+local function turnRightAction(ws)
+    turnAction(ws, "turn_right")
+end
+
+local function moveAction(ws, direction)
+    local func = action_tbl[direction]
+    local canMove, msg = func()
+    local text = direction .. "|" .. tostring(canMove)
+    ws.send(concatMessage(text, msg))
+end
+
+local function moveForward(ws)
+    moveAction(ws, "forward")
+end
+
+local function moveBack(ws)
+    moveAction(ws, "back")
+end
+
+local function moveUp(ws)
+    moveAction(ws, "up")
+end
+
+local function moveDown(ws)
+    moveAction(ws, "down")
+end
+
+local function digAction(ws, direction)
+    local funcs = action_tbl[direction]
+    local isBlock, data = funcs[1]()
+    if not isBlock then
+        return
+    end
+    local success, msg = funcs[2]()
+    local text = direction .. "|" .. tostring(success)
+    ws.send(concatMessage(text, msg))
+end
+
+local function digDownAction(ws)
+    digAction(ws, "dig_down")
+end
+
+local function digUpAction(ws)
+    digAction(ws, "dig_up")
+end
+
+local function digForwardAction(ws)
+    digAction(ws, "dig")
+end
+
+local function placeAction(ws, direction)
+    local func = action_tbl[direction]
+    local detail = turtle.getItemDetail()
+    local placed, msg = func()
+    if detail == nil then
+        detail = {}
+    end
+    local text = direction .. "|" .. textutils.serialiseJSON(detail)
+    ws.send(concatMessage(text, msg))
+end
+
+local function placeDownAction(ws)
+    placeAction(ws, "place_down")
+end
+
+local function placeUpAction(ws)
+    placeAction(ws, "place_up")
+end
+
+local function placeForwardAction(ws)
+    placeAction(ws, "place")
+end
+
 func_tbl = {
     ["handshake"] = function(ws)
         print("Handshake Established!")
         ws.send("handshake|Yes")
     end,
     ["inspect"] = function(ws)
-        local exists, data = turtle.inspect()
-        local msg = "inspect|" .. textutils.serialiseJSON(data)
-        ws.send(msg)
+        inspectForward(ws)
     end,
     ["inspect_up"] = function(ws)
-        local exists, data = turtle.inspectUp()
-        local msg = "inspect_up|" .. textutils.serialiseJSON(data)
-        ws.send(msg)
+        inspectUp(ws)
     end,
     ["inspect_down"] = function(ws)
-        local exists, data = turtle.inspectDown()
-        local msg = "inspect_down|" .. textutils.serialiseJSON(data)
-        ws.send(msg)
+        inspectDown(ws)
     end,
     ["turn_left"] = function(ws)
-        local turn, msg = turtle.turnLeft()
-        local text = "turn_left|" .. tostring(turn)
-        if msg ~= nil then
-            text = text .. '|' .. msg
-        end
-        ws.send(text)
+        turnLeftAction(ws)
     end,
     ["turn_right"] = function(ws)
-        local turn, msg = turtle.turnRight()
-        local text = "turn_right|" .. tostring(turn)
-        if msg ~= nil then
-            text = text .. '|' .. msg
-        end
-        ws.send(text)
+        turnRightAction(ws)
     end,
     ["forward"] = function(ws)
-        local canMove, msg = turtle.forward()
-        local text = "forward|" .. tostring(canMove)
-        if msg ~= nil then
-            text = text .. '|' .. msg
-        end
-        ws.send(text)
+        moveForward(ws)
     end,
     ["back"] = function(ws)
-        local canMove, msg = turtle.back()
-        local text = "back|" .. tostring(canMove)
-        if msg ~= nil then
-            text = text .. '|' .. msg
-        end
-        ws.send(text)
+        moveBack(ws)
     end,
     ["up"] = function(ws)
-        local canMove, msg = turtle.up()
-        local text = "up|" .. tostring(canMove)
-        if msg ~= nil then
-            text = text .. '|' .. msg
-        end
-        ws.send(text)
+        moveUp(ws)
     end,
     ["down"] = function(ws)
-        local canMove, msg = turtle.down()
-        local text = "down|" .. tostring(canMove)
-        if msg ~= nil then
-            text = text .. '|' .. msg
-        end
-        ws.send(text)
+        moveDown(ws)
     end,
     ["dig | True"] = function(ws)
-        local isBlock, data = turtle.inspect()
-        if not isBlock then
-            return
-        end
-        local success, msg = turtle.dig()
-        local text = "dig|" .. tostring(success)
-        if msg ~= nil then
-            text = text .. "|" .. msg
-        end
-        ws.send(text)
+        digForwardAction(ws)
     end,
     ["dig_up | True"] = function(ws)
-        local isBlock, data = turtle.inspectUp()
-        if not isBlock then
-            return
-        end
-        local success, msg = turtle.digUp()
-        local text = "dig_up|" .. tostring(success)
-        if msg ~= nil then
-            text = text .. "|" .. msg
-        end
-        ws.send(text)
+        digUpAction(ws)
     end,
     ["dig_down | True"] = function(ws)
-        local isBlock, data = turtle.inspectDown()
-        if not isBlock then
-            return
-        end
-        local success, msg = turtle.digDown()
-        local text = "dig_down|" .. tostring(success)
-        if msg ~= nil then
-            text = text .. "|" .. msg
-        end
-        ws.send(text)
-    end
+        digDownAction(ws)
+    end,
+    ["place"] = function(ws)
+        placeForwardAction(ws)
+    end,
+    ["place_up"] = function(ws)
+        placeUpAction(ws)
+    end,
+    ["place_down"] = function(ws)
+        placeDownAction(ws)
+    end,
 }
- 
-local function connect()
-    return http.websocket("ws://0.0.0.0:8000")
-end
- 
-local ws = connect()
-if not ws then
-    print("Failed to connect websocket!")
-    return
-end
- 
-local function selectSlot(slot)
-    turtle.select(slot)
-end
- 
-local function handleInventoryChange(msg)
+
+local function handleInventoryChange(ws, msg)
     for s in string.gmatch(msg, "%S+") do
         local num = tonumber(s) or -1
-        if num ~= -1 then
-            selectSlot(num)
+        if num ~= nil and num ~= -1 then
+            print(num)
+            turtle.select(num)
             break
         end
     end
+
     detail = turtle.getItemDetail()
     text = "select_slot|"
+
     if detail ~= nil then
         text = text .. textutils.serialiseJSON(detail)
     else
         text = text .. "Empty"
     end
+
     ws.send(text)
 end
- 
+
+local ws, err = http.websocket("ws://0.0.0.0:8000")
+if err then
+    print(err)
+    return
+end
+
+local function fetch(ws)
+    return 
+end
+
+term.clear()
+term.setCursorPos(1, 1)
+print("   PyTurtle OS {o_o}\n")
+
 while true do
-    local event, url, msg, b = os.pullEvent()
- 
-    if event ~= "websocket_message" then
+    local msg = ws.receive()
+
+    if msg == nil then
+        print("Failed to fetch...")
+        os.sleep(5)
         goto continue
     end
- 
-    local func = func_tbl[msg]
+
     print(msg)
+
+    local func = func_tbl[msg]
+
     if string.find(msg, "select_slot") then
-        handleInventoryChange(msg)
+        handleInventoryChange(ws, msg)
     else
         if func then
             func(ws)
         end
     end
- 
+
     ::continue::
-    -- os.sleep(0.5)
 end
+
+ws.close()
