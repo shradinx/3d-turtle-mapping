cdd6f04cc1feca0ae4a5804ad882977e4d41ecad
Shradinx <luphcore@gmail.com>
2025-06-03 17:02:56 -0400

    move start_websocket -> async_util script

--- src/async_util.py
+++ src/async_util.py
@@ -1,19 +1,19 @@
 from imports import *
 
-import turtle_handler
-import block_voxel as bv
+import turtle_handler as th
 import block_util as bu
 import json_util as ju
+import gui_util as gu
 
 active_websocket = None
 
 movementActions = {
-    "turn_left": [turtle_handler.rotate, True],
-    "turn_right": [turtle_handler.rotate, False],
-    "forward": [turtle_handler.moveForwardBack, True, ""],
-    "back": [turtle_handler.moveForwardBack, False, ""],
-    "up": [turtle_handler.moveUpDown, True, ""],
-    "down": [turtle_handler.moveUpDown, False, ""]
+    "turn_left": [th.rotate, True],
+    "turn_right": [th.rotate, False],
+    "forward": [th.moveForwardBack, True, ""],
+    "back": [th.moveForwardBack, False, ""],
+    "up": [th.moveUpDown, True, ""],
+    "down": [th.moveUpDown, False, ""]
 }
 
 buttonActions = {
@@ -32,15 +32,15 @@ buttonActions = {
 WIP_actions = {}
 
 blockBreakActions = {
-    "dig": [turtle_handler.dig, turtle_handler.Direction.FORWARD],
-    "dig_up": [turtle_handler.dig, turtle_handler.Direction.UP],
-    "dig_down": [turtle_handler.dig, turtle_handler.Direction.DOWN]
+    "dig": [th.dig, th.Direction.FORWARD],
+    "dig_up": [th.dig, th.Direction.UP],
+    "dig_down": [th.dig, th.Direction.DOWN]
 }
 
 blockPlaceActions = {
-    "place": [turtle_handler.place, turtle_handler.Direction.FORWARD],
-    "place_up": [turtle_handler.place, turtle_handler.Direction.UP],
-    "place_down": [turtle_handler.place, turtle_handler.Direction.DOWN]
+    "place": [th.place, th.Direction.FORWARD],
+    "place_up": [th.place, th.Direction.UP],
+    "place_down": [th.place, th.Direction.DOWN]
 }
 
 inspectActions = {
@@ -50,7 +50,7 @@ inspectActions = {
 }
 
 invActions = {
-    "select_slot": turtle_handler.updateSlotInfo
+    "select_slot": th.updateSlotInfo
 }
 
 ec = None
@@ -65,6 +65,36 @@ def get_active_websocket():
 def set_active_websocket(ws: websockets.ServerConnection):
     global active_websocket
     active_websocket = ws
+
+host = "0.0.0.0"
+port = 8000
+
+def start_websocket(loop: asyncio.AbstractEventLoop, actionMenu: Entity):
+    async def run():
+        async with websockets.serve(handshake, host, port) as ws:
+            print(f"[WebSocket] Server started on ws://{host}:{port}")
+
+            def make_buttons():
+                pos = (window.top_left.x+0.25, window.top_left.y)
+                gu.populate_action_text()
+
+                WindowPanel(
+                    parent=actionMenu,
+                    title="Actions Menu",
+                    position=pos,
+                    content=[
+                        *gu.get_action_text().values(),
+                        *gu.get_buttons(loop=loop)
+                    ]
+                )
+                gu.center_action_text()
+            
+            invoke(make_buttons, delay=0.1)
+            invoke(th.make_inventory, delay=0.1)
+            init_editor_cam()
+            await asyncio.Future()
+
+    loop.run_until_complete(run())
 
 
 async def handshake(websocket: websockets.ServerConnection):
@@ -107,9 +137,9 @@ async def handshake(websocket: websockets.ServerConnection):
         active_websocket = None
 
 def handle_handshake():
-    t = turtle_handler.get_selected_turtle()
+    t = th.get_selected_turtle()
     coords = t.getXZCoords(True) if t is not None else (0, 0, 0)
-    turtle_handler.create_turtle(ec, coords)
+    th.create_turtle(ec, coords)
 
 def sendActionAsync(coroutine, loop: asyncio.AbstractEventLoop):
     return asyncio.run_coroutine_threadsafe(coroutine, loop)
@@ -118,7 +148,7 @@ async def sendAction(action: str, delay=None):
     if not active_websocket:
         return
     
-    if (action in movementActions) and turtle_handler.get_animation_active():
+    if (action in movementActions) and th.get_animation_active():
         return
     
     if delay is not None:
@@ -131,19 +161,19 @@ async def sendAction(action: str, delay=None):
         block = None
 
         if len(split) == 1:
-            block = turtle_handler.get_selected_turtle().getBlockInFront()
+            block = th.get_selected_turtle().getBlockInFront()
         else:
             dir = split[1].upper()
-            dirEnum = turtle_handler.Direction(dir)
+            dirEnum = th.Direction(dir)
             if dirEnum is None:
                 return
             
-            block = turtle_handler.getBlock(dirEnum)
+            block = th.getBlock(dirEnum)
 
         msg = msg + " | " + str((block is not None))
         await active_websocket.send(msg)
     elif re.search("auto_inspect", action):
-        await turtle_handler.autoInspect()
+        await th.autoInspect()
     else:
         await active_websocket.send(action)
 
@@ -163,13 +193,13 @@ def handle_action(action: str, data: str):
         return True
     elif action in blockBreakActions.keys():
         func, dir = blockBreakActions[action]
-        block = turtle_handler.getBlock(dir)
+        block = th.getBlock(dir)
         func(block)
 
         return True
     elif action in blockPlaceActions.keys():
         func, dir = blockPlaceActions[action]
-        option = turtle_handler.getDirectionOption(dir)
+        option = th.getDirectionOption(dir)
 
         value = ju.getValueFromJSON(data, "count")
         if value is None:
@@ -177,7 +207,7 @@ def handle_action(action: str, data: str):
         
         newData = "Empty" if value-1 <= 0 else ju.modifyJSON(data, "count", int(value)-1, dumpToString=True)
 
-        coords = turtle_handler.getCoords(dir, option)
+        coords = th.getCoords(dir, option)
         func(data, newData, coords)
 
         return True
--- src/main.py
+++ src/main.py
@@ -2,15 +2,12 @@ import block_voxel as bv
 import async_util as au
 import threading
 
-import turtle_handler as th
-import gui_util as gu
-
 from imports import *
 
 app = Ursina(
     title="Interactive 3D Environment: Integration with ComputerCraft Turtles",
     icon="resources/pyturtle.ico",
-    size=(1280, 720), 
+    size=(1280, 720),
     borderless=False)
 app.backfaceCullingOn()
 
@@ -23,8 +20,6 @@ window.cog_button.disable()
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-host = "0.0.0.0"
-port = 8000
 
 actionMenu = Entity(parent=camera.ui)
 inventoryMenu = Entity(parent=camera.ui)
@@ -54,36 +49,8 @@ def input(key):
         au.sendActionAsync(au.sendAction(inputs[key]), loop)
 
 
-def start_websocket():
-    async def run():
-        async with websockets.serve(au.handshake, host, port) as ws:
-            print(f"[WebSocket] Server started on ws://{host}:{port}")
-
-            def make_buttons():
-                pos = (window.top_left.x+0.25, window.top_left.y)
-                gu.populate_action_text()
-
-                WindowPanel(
-                    parent=actionMenu,
-                    title="Actions Menu",
-                    position=pos,
-                    content=[
-                        *gu.get_action_text().values(),
-                        *gu.get_buttons(loop=loop)
-                    ]
-                )
-                gu.center_action_text()
-            
-            invoke(make_buttons, delay=0.1)
-            invoke(th.make_inventory, delay=0.1)
-            au.init_editor_cam()
-            await asyncio.Future()
-
-    loop.run_until_complete(run())
-
-
 def main():
-    ws_thread = threading.Thread(target=start_websocket, daemon=True)
+    ws_thread = threading.Thread(target=Func(au.start_websocket, loop, actionMenu), daemon=True)
     ws_thread.start()
 
     app.run()
